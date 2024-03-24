0c1943e3987aae0b3bcb47da7016aec727350d17
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-03-25 00:29:02 +0600

    add: Bubble Sort Implementation Descending order

--- js/sorting-algorithms.js
+++ js/sorting-algorithms.js
@@ -21,4 +21,35 @@ function funBubbleSort(arr) {
   }
   return arr;
 }
-funBubbleSort([5, 9, 11, 4]);
+// Define an array
+var arr = [5, 3, 8, 1, 2];
+
+// Call the funBubbleSort function and pass the array as an argument
+var sortedArray = funBubbleSort(arr);
+
+// Output the sorted array
+console.log(sortedArray);
+
+//JavaScript Bubble Sort Implementation Descending order
+function funBubbleSortDescending(arr) {
+  for (var i = arr.length - 1; i > 0; i--) {
+    for (var j = 0; j < i; j++) {
+      if (arr[j] < arr[j + 1]) {
+        // Modified condition for descending order
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// Define an array
+var arr = [5, 3, 8, 1, 2];
+
+// Call the funBubbleSortDescending function and pass the array as an argument
+var sortedArray = funBubbleSortDescending(arr);
+
+// Output the sorted array
+console.log(sortedArray);
