57b6454893e3a63e3789ffd3643a204d819acde6
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-04-03 23:47:31 +0600

    add : Heap sort definition learn

--- js/sorting-algorithms.js
+++ js/sorting-algorithms.js
@@ -153,3 +153,16 @@ const arrayMerge = [8, 3, 9, 5, 1, 7, 2, 4, 6];
 console.log("Original array:", arrayMerge);
 const sortedArray = mergeSort(arrayMerge);
 console.log("Sorted array:", sortedArray);
+
+// Heap sort is a comparison-based sorting technique based on Binary Heap data structure. It is similar to the selection sort where we first find the minimum element and place the minimum element at the beginning. Repeat the same process for the remaining elements.
+
+// To solve the problem follow the below idea:
+
+// First convert the array into heap data structure using heapify, then one by one delete the root node of the Max-heap and replace it with the last node in the heap and then heapify the root of the heap. Repeat this process until size of heap is greater than 1.
+
+// Build a heap from the given input array.
+// Repeat the following steps until the heap contains only one element:
+// Swap the root element of the heap (which is the largest element) with the last element of the heap.
+// Remove the last element of the heap (which is now in the correct position).
+// Heapify the remaining elements of the heap.
+// The sorted array is obtained by reversing the order of the elements in the input array.
